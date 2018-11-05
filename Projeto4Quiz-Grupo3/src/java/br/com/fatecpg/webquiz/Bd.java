ac7958633135c03880e64db8796d40a9155babeb
Thyago Santos de Araujo <thyagopcb.araujo@gmail.com>
2018-11-05 10:22:55 -0200

    Adicionado get/sets na classe question - Adicionado o array com as questões, respostas e alternativas

--- Projeto4Quiz-Grupo3/src/java/br/com/fatecpg/webquiz/Bd.java
+++ Projeto4Quiz-Grupo3/src/java/br/com/fatecpg/webquiz/Bd.java
@@ -5,10 +5,19 @@
  */
 package br.com.fatecpg.webquiz;
 
+import java.util.ArrayList;
+
 /**
  *teste
  * @author Daniel Lucas
  */
 public class Bd {
+    public static ArrayList<Question> getQuiz(){
+        ArrayList<Question> quiz = new ArrayList<>();
+        quiz.add(new Question ("5+5?", "10", new String[]{"10","30","15"}));
+        quiz.add(new Question ("2*1?", "2", new String[]{"1","2","3"}));
+        quiz.add(new Question ("5*4?", "20", new String[]{"40","25","20"}));
+        return quiz;
+    }
     
 }
