7242df76cabd3f9398a5078b07cc754cf9ad4131
Thyago Santos de Araujo <thyagopcb.araujo@gmail.com>
2018-11-08 09:32:21 -0200

    Comentado a ordenação no array users para testes

--- Projeto4Quiz-Grupo3/src/java/br/com/fatecpg/webquiz/Bd.java
+++ Projeto4Quiz-Grupo3/src/java/br/com/fatecpg/webquiz/Bd.java
@@ -26,44 +26,44 @@ public class Bd {
         
        if (hist.isEmpty()){
         Historicos p1 = new Historicos();
-        p1.setUser("teste",7.0);
+        p1.setUser("teste1",7.0);
         hist.add(p1);
         
         
         Historicos p2 = new Historicos();
-        p2.setUser("teste",7.5);
+        p2.setUser("teste2",7.5);
         hist.add(p2);
         
         Historicos p3 = new Historicos();
-        p3.setUser("teste",8.0);
+        p3.setUser("teste3",8.0);
         hist.add(p3);
         
         Historicos p4 = new Historicos();
-        p4.setUser("teste",8.5);
+        p4.setUser("teste4",8.5);
         hist.add(p4);
         
         
         Historicos p5 = new Historicos();
-        p5.setUser("teste",6.0);
+        p5.setUser("teste5",6.0);
         hist.add(p5);
         
        Historicos p6 = new Historicos();
-        p6.setUser("teste",7.0);
+        p6.setUser("teste6",7.0);
         hist.add(p6);
         
        Historicos p7 = new Historicos();
-        p7.setUser("teste",8.0);
+        p7.setUser("teste7",8.0);
         hist.add(p7);
         
         Historicos p8 = new Historicos();
-        p8.setUser("teste",4.0);
+        p8.setUser("teste8",4.0);
         hist.add(p8);
         
         Historicos p9 = new Historicos();
-        p9.setUser("teste",5.0);
+        p9.setUser("teste9",5.0);
        hist.add(p9);        
        }
-      Collections.sort(hist, Collections.reverseOrder());
+      //Collections.sort(hist, Collections.reverseOrder());
        return hist;
        }
     private static final ArrayList<Usuarios> user = new ArrayList<>();         
